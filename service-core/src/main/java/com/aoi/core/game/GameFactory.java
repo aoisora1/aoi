b54cc23fff2f9a498f3b8361cad63a9339a73e9a
aoisora1 <1317474892@qq.com>
2025-08-05 18:03:01 +0800

    池化对局

--- service-core/src/main/java/com/aoi/core/game/GameFactory.java
+++ service-core/src/main/java/com/aoi/core/game/GameFactory.java
@@ -7,6 +7,7 @@ import org.springframework.stereotype.Service;
 @Service
 public class GameFactory {
 
+    // TODO 开闭
     public Game newGame(StartContext context) {
         GameEnum gameEnum = GameEnum.getGameEnum(context.getCode());
         if (gameEnum == null) {
